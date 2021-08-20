6ba51339d5b85bd0e77c19cbcb256c390672e20e
borislavstoychev <stoy4ew@gmail.com>
2021-08-20 16:22:03 +0300

    edit admin.py include is_active to user

--- nails_project/accounts/admin.py
+++ nails_project/accounts/admin.py
@@ -14,13 +14,13 @@ UserModel = get_user_model()
 
 @admin.register(UserModel)
 class NailsUserAdmin(UserAdmin):
-    list_display = ('email', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'groups')
+    list_display = ('email', 'is_staff', "is_active")
+    list_filter = ('is_staff', 'is_superuser', 'groups', "is_active")
     ordering = ('date_joined',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Permissions', {
-            'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            'fields': ('is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions'),
         }),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
